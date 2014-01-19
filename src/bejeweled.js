a979af9a2354d1697d142289914ad9abe53656ab
Nejuf <nejuf1@gmail.com>
2014-01-18 20:44:55 -0800

    Adds game canvas using Phaser

---
+++ src/bejeweled.js
@@ -0,0 +1,21 @@
+(function(){ 
+
+	var game = new Phaser.Game(640, 480, Phaser.AUTO, 'bejeweled-container', {
+		preload: preload,
+		create: create,
+		update: update
+	});
+
+	function preload(){
+
+	}
+
+	function create(){
+
+	}
+
+	function update(){
+
+	}
+	
+}).call(this);
